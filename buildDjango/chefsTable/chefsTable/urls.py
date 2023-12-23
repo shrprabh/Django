f100b6735be547371abbaef097fdd5a96aea3b9d
shreyasprabhakar <shrprabh@ttu.edu>
2023-12-22 23:43:12 -0600

    feat(adding url): adding route directly from the neew homeapp

--- buildDjango/chefsTable/chefsTable/urls.py
+++ buildDjango/chefsTable/chefsTable/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from homeapp import views
 urlpatterns = [
+    path('',views.home,name="Home"),
     path('demo/',include('demoapp.urls')),
     path('admin/', admin.site.urls)
 ]
--- buildDjango/chefsTable/homeapp/views.py
+++ buildDjango/chefsTable/homeapp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello world this is index view of demo app")
+def home(request):
+    return HttpResponse("Hello Home")
